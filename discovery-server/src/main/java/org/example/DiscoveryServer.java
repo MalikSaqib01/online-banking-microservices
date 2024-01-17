a03f019508b7516e9218eed6069a63f4f0bd9ca3
todolist-backend <malicksaqib01@gmail.com>
2024-01-17 15:22:59 +0500

    adding new things

--- discovery-server/src/main/java/org/example/DiscoveryServer.java
+++ discovery-server/src/main/java/org/example/DiscoveryServer.java
@@ -3,8 +3,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServer {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServer.class,args);
